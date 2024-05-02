e92853c76980dc0969297a132ffc3209775c6adf
dongki <ehdgusdl0816@gmail.com>
2024-05-02 17:07:24 +0900

    0502-1

--- src/nested/local/LocalOuterV3.java
+++ src/nested/local/LocalOuterV3.java
@@ -1,5 +1,7 @@
 package nested.local;
 
+import java.lang.reflect.Field;
+
 public class LocalOuterV3 {
     private int outInstanceVar = 3;
 
@@ -33,5 +35,9 @@ public class LocalOuterV3 {
 
         //추가
         System.out.println("필드 확인");
+        Field[] fields = printer.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println("field = " + field);
+        }
     }
 }
