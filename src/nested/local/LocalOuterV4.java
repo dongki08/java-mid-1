bb8851c0e50582ec59de08ce13bc6f4b4ec9d46f
dongki <ehdgusdl0816@gmail.com>
2024-05-03 14:04:25 +0900

    0503-1

--- src/nested/local/LocalOuterV4.java
+++ src/nested/local/LocalOuterV4.java
@@ -2,10 +2,10 @@ package nested.local;
 
 import java.lang.reflect.Field;
 
-public class LocalOuterV3 {
+public class LocalOuterV4 {
     private int outInstanceVar = 3;
 
-    public Printer process(int paramVar) {
+    public Printer process(final int paramVar) {
 
         int localVar = 1; // 지역 변수는 스택 프레임이 종료되는 순간 함께 제거된다.
 
@@ -24,12 +24,14 @@ public class LocalOuterV3 {
         }
 
         LocalPrinter printer = new LocalPrinter();
-
+        // 만약 localVar의 값을 변경한다면? 다시 캡펴해야 하나?
+        // localVar = 10;
+        // paramVar = 20;
         return printer;
     }
 
     public static void main(String[] args) {
-        LocalOuterV3 localOuter = new LocalOuterV3();
+        LocalOuterV4 localOuter = new LocalOuterV4();
         Printer printer = localOuter.process(2);
         //printer.print()를 나중에 실행한다. process()의 스택 프레임이 사라진 이후에 실행한다
         printer.print();
