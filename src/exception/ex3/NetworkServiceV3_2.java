9ca46331057dc005b432ceae364021b6bf6ccfd9
dongki <ehdgusdl0816@gmail.com>
2024-05-16 12:40:16 +0900

    0516-1 예외처리(공통예외처리 unCheck)

--- src/exception/ex3/NetworkServiceV3_2.java
+++ src/exception/ex3/NetworkServiceV3_2.java
@@ -1,10 +1,12 @@
 package exception.ex3;
 
 
-import exception.ex3.exception.ConnectExceptionV3;
-import exception.ex3.exception.SendExceptionV3;
 
-public class NetworkServiceV3_1 {
+import exception.ex3.exception.ConnectExceptionV3;
+import exception.ex3.exception.NetworkClientExceptionV3;
+
+
+public class NetworkServiceV3_2 {
     public void sendMessage(String data) {
         String address = "http://exanple.com";
         NetworkClientV3 client = new NetworkClientV3(address);
@@ -15,8 +17,10 @@ public class NetworkServiceV3_1 {
             client.send(data);
         } catch (ConnectExceptionV3 e) {
             System.out.println("[연결 오류] 주소 : " + e.getAddress() + ", 메세지 : " + e.getMessage());
-        } catch (SendExceptionV3 e) {
-            System.out.println("[전송 오류] 전송 데이터 : " + e.getSendData() + ", 메세지 : " + e.getMessage());
+        } catch (NetworkClientExceptionV3 e) {
+            System.out.println("[네트워크 오류] 메시지 : " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("[알 수 없는 오류] 메시지 : " + e.getMessage());
         } finally {
             client.disconnect();
         }
